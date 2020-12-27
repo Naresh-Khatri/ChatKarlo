653d635252174d6ea2ce75eda6c88a5262466eb2
Naresh <naresh.khatri2345@gmail.com>
2020-12-27 16:19:18 +0530

    asdfasdfasdfasdfas

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express()
+const http = require("http").Server(app);
+const socketio = require("socket.io")(http, { cors: { origin: "*" } });
+
+var count = 0;
+app.use(express.static('public'))
+
+const PORT = process.env.PORT || 8081
+
+socketio.on("connection", socket => {
+  count++;
+  console.log(`a user connected`);
+  socketio.emit("counter", { count: count });
+  console.log("connected count", count);
+
+  socket.on("disconnect", ()=>{
+    count--;
+    console.log("user disconnected");
+    console.log("disconnected count", count);
+  });
+
+  socket.on("message", message => {
+    console.log(`${socket.id.substr(0, 2)} said ${JSON.stringify(message)}`);
+    socketio.emit("message", JSON.stringify(message));
+  });
+});
+
+
+http.listen(PORT, () => console.log("listening on port " + PORT));
