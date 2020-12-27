7de593ee781a854c6fac8598231838f4d04583ec
Naresh <naresh.khatri2345@gmail.com>
2020-12-27 17:04:04 +0530

    asdfasdfasdfasdfas8

--- server.js
+++ server.js
@@ -7,14 +7,8 @@ const socketio = require("socket.io")(http, { cors: { origin: "*" } });
 var count = 0;
 
 
+app.use(express.static(path.join(__dirname, "dist", "spa")));
 
-app.use((req, res, next) => {
-  // return res.send(req.path);
-
-  if(req.path == "/") return res.sendFile("index.html", { root : path.join(__dirname, "dist/spa") });
-
-  res.sendFile(req.path,  { root : path.join(__dirname, "dist/spa") });
-});
 
 const PORT = process.env.PORT || 8081
 
